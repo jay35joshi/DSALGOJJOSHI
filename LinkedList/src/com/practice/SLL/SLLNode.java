309c0afaf47497a4609e838fcbefe56dda126464
jjoshi <jayjoshi35@gmail.com>
2019-11-10 22:20:43 +0530

    Linked List

--- LinkedList/src/com/practice/SLL/SLLNode.java
+++ LinkedList/src/com/practice/SLL/SLLNode.java
@@ -1,5 +1,22 @@
 package com.practice.SLL;
 
 public class SLLNode {
+private int data;
+private SLLNode next;
 
+SLLNode(int data){
+	this.data = data;
+}
+public int getData() {
+	return data;
+}
+public void setData(int data) {
+	this.data = data;
+}
+public SLLNode getNext() {
+	return next;
+}
+public void setNext(SLLNode next) {
+	this.next = next;
+}
 }
